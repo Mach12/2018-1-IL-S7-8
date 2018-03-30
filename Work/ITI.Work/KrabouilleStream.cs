f867540e4cc8af3ef0248a507f6e6b8fc08bb112
Pixelmod <guillaumedanguin@yahoo.fr>
2018-03-30 09:15:45 +0200

    Something that resembles a cryptostream

--- Work/ITI.Work/KrabouilleStream.cs
+++ Work/ITI.Work/KrabouilleStream.cs
@@ -22,41 +22,72 @@ namespace ITI.Work
     {
         public KrabouilleStream( Stream inner, KrabouilleMode mode, string password )
         {
+            if( String.IsNullOrEmpty( password ) ) throw new ArgumentException();
+
+            _inner = inner ?? throw new ArgumentNullException();
+            _mode = mode;
+            _password = password;
         }
 
-        public override bool CanRead => throw new NotImplementedException();
+        private Stream _inner;
+        private KrabouilleMode _mode;
+        private string _password;
 
-        public override bool CanSeek => throw new NotImplementedException();
+        public override bool CanRead => _mode == KrabouilleMode.Unkrabouille && _inner.CanRead;
 
-        public override bool CanWrite => throw new NotImplementedException();
+        public override bool CanSeek => false;
 
-        public override long Length => throw new NotImplementedException();
+        public override bool CanWrite => _mode == KrabouilleMode.Krabouille && _inner.CanWrite;
 
-        public override long Position { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public override long Length => _inner.Length;
+
+        public override long Position { get => throw new NotSupportedException(); set => throw new NotSupportedException(); }
+
+        private byte PasswordAt( int index )
+        {
+            return (byte)_password[ index % _password.Length ];
+        }
 
         public override void Flush()
         {
-            throw new NotImplementedException();
+            _inner.Flush();
         }
 
         public override int Read( byte[] buffer, int offset, int count )
         {
-            throw new NotImplementedException();
+            int actualCount = _inner.Read( buffer, offset, count );
+
+            for( int i = 0; i < actualCount; ++i )
+            {
+                byte keyByte = PasswordAt( offset + i );
+                buffer[ i ] ^= keyByte;
+            }
+
+            return actualCount;
         }
 
         public override long Seek( long offset, SeekOrigin origin )
         {
-            throw new NotImplementedException();
+            throw new NotSupportedException();
         }
 
         public override void SetLength( long value )
         {
-            throw new NotImplementedException();
+            _inner.SetLength( value );
         }
 
         public override void Write( byte[] buffer, int offset, int count )
         {
-            throw new NotImplementedException();
+            byte[] newBuf = new byte[count];
+            Array.Copy( buffer, newBuf, count );
+
+            for( int i = 0; i < count; ++i )
+            {
+                byte keyByte = PasswordAt( offset + i );
+                newBuf[ i ] = (byte)(keyByte^buffer[i]);
+            }
+
+            _inner.Write( buffer, offset, count );
         }
     }
 }
