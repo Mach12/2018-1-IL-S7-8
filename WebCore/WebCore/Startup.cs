b7a127ef31f71551134e7bab859f1c6136c9c227
Olivier Spinelli <olivier.spinelli@invenietis.com>
2018-05-16 12:49:00 +0200

    Entering configuration.

--- WebCore/WebCore/Startup.cs
+++ WebCore/WebCore/Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
@@ -11,8 +12,10 @@ namespace WebCore
 {
     class Startup
     {
-        public void ConfigureServices( IServiceCollection services )
+        public void ConfigureServices( IServiceCollection services, IConfiguration configuration )
         {
+            services.AddOptions();
+            services.Configure<WinOrLooseOptions>( configuration.GetSection( "WinOrLoose" ) );
             services.AddScoped<IWinOrLooseService, WinOrLooseUserConsentService>();
         }
 
