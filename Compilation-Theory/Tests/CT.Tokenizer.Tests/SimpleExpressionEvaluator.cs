e5b20402b6a891e55e4ef4edf71ccfafa1251a23
Olivier Spinelli <olivier.spinelli@invenietis.com>
2018-07-09 15:16:41 +0200

    Simple tests.

--- Compilation-Theory/Tests/CT.Tokenizer.Tests/SimpleExpressionEvaluator.cs
+++ Compilation-Theory/Tests/CT.Tokenizer.Tests/SimpleExpressionEvaluator.cs
@@ -1,19 +1,35 @@
-﻿using System;
+﻿using FluentAssertions;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using Xunit;
 
 namespace CT.Tokenizer.Tests
 {
 
     public class SimpleExpressionEvaluator
     {
+        
+        [Theory]
+        [InlineData("3", 3)]
+        [InlineData("3 + 7", 3 + 7)]
+        [InlineData("(3 + 7)*8", (3 + 7) * 8)]
+        public void simple_exprexxion_evaluation( string text, int result)
+        {
+            Evaluate(text).Should().Be(result);
+        }
 
 
         static int Evaluate(string text) => EvalExpression(new Tokenizer(text));
 
-        static int EvalExpression(Tokenizer t ) 
-        {
 
+        /// <summary>
+        /// expression → terme  opérateur-additif  expression  |  terme 
+        /// </summary>
+        /// <param name="t"></param>
+        /// <returns></returns>
+        static int EvalExpression(Tokenizer t)
+        {
         }
 
     }
