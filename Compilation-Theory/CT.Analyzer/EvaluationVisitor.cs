5cc5b7d0168b65b20a36a1ca494d897c5bd9d04c
Olivier Spinelli <olivier.spinelli@invenietis.com>
2018-07-10 16:41:30 +0200

    EvaluationVisitor to do.

---
+++ Compilation-Theory/CT.Analyzer/EvaluationVisitor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CT
+{
+    public class EvaluationVisitor : NodeVisitor
+    {
+        public int Result { get; private set; }
+
+    }
+}
---
+++ Compilation-Theory/Tests/CT.Analyser.Tests/EvaluationVisitorTests.cs
@@ -0,0 +1,37 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace CT.Tests
+{
+
+    public class EvaluationVisitorTests
+    {
+
+        [Theory]
+        [InlineData("3", 3)]
+        [InlineData("3 + 7", 3 + 7)]
+        [InlineData("(3 + 7)*8", (3 + 7) * 8)]
+        [InlineData("(3 + 7)*(8-9)", (3 + 7) * (8 - 9))]
+        [InlineData("3 * 1 + 2", 3 * 1 + 2)]
+        [InlineData("1 - 1 + 2", 1 - 1 + 2)]
+        [InlineData("2 * 3 * 4 + 1", 2 * 3 * 4 + 1)]
+        [InlineData("4 / 2 * 3", 4 / 2 * 3)]
+        [InlineData("3 + 5 * 125 / 7 - 6 + 10 ", 3 + 5 * 125 / 7 - 6 + 10)]
+
+        [InlineData("-3 + 5 *-6", -3 + 5 * -6)]
+        [InlineData("1*-3+-7*5", 1 * -3 + -7 * 5)]
+        [InlineData("-5*-4+-2", -5 * -4 + -2)]
+        [InlineData("-5*-(4+8*-(2+5))+-2", -5 * -(4 + 8 * -(2 + 5)) + -2)]
+        public void evaluating_constants_only(string text, int result)
+        {
+            var ast = Analyzer.Parse(text);
+            var v = new EvaluationVisitor();
+            v.Visit(ast);
+            v.Result.Should().Be(result);
+        }
+
+    }
+}
